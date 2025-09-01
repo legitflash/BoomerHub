13509a2f0e12a1532ace9e1b74e4473af07fcf56
Uduafemhe Favour <uduafemhe.A.favour@gmail.com>
2025-09-01 14:17:21 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   staticPageGenerationTimeout: 1000,
-  serverActions: {
-    bodySizeLimit: '10mb',
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -80,3 +77,5 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+    
