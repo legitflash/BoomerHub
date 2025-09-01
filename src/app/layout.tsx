c5049ea6f3b157cd03bfa72b8cfe0698db11910b
Uduafemhe Favour <uduafemhe.A.favour@gmail.com>
2025-09-01 01:03:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
@@ -18,14 +19,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="d91221cdd32ab4af19a78603576153e265e34cd7" content="d91221cdd32ab4af19a78603576153e265e34cd7" />
         <meta name="monetag" content="b94362744cd3ba95e527b00f595826f5" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <script src="https://couphaithuph.net/act/files/tag.min.js?z=9805952" data-cfasync="false" async></script>
       </head>
       <body className="font-body antialiased">
           <AuthProvider>
@@ -36,7 +36,10 @@ export default function RootLayout({
             </div>
             <Toaster />
           </AuthProvider>
-          <script dangerouslySetInnerHTML={{ __html: `(s=>{s.dataset.zone=9805964,s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))` }} />
+          <Script id="monetag-push" strategy="afterInteractive" src="https://couphaithuph.net/act/files/tag.min.js?z=9805952" data-cfasync="false" async />
+          <Script id="monetag-vignette" strategy="afterInteractive">
+            {`(s=>{s.dataset.zone=9805964,s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
+          </Script>
       </body>
     </html>
   );
