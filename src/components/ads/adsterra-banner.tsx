caf2ad14c42409d820891a046dbfe00b14f9c22e
Uduafemhe Favour <uduafemhe.A.favour@gmail.com>
2025-09-03 12:45:12 +0000

    Updatec native ads Adsterra,  1<script async="async" data-cfasync="false

--- src/components/ads/adsterra-banner.tsx
+++ src/components/ads/adsterra-banner.tsx
@@ -5,11 +5,11 @@ import Script from 'next/script';
 export default function AdsterraBanner() {
   return (
     <div className="my-12 flex justify-center">
-      <div id="container-f3f0d27c1b811dd6ad39758bea4c8f40"></div>
+      <div id="container-2c5e03b2597c3bd82333c06d7e9dd6e0"></div>
       <Script
         async
         data-cfasync={false}
-        src="//chickenadjacent.com/f3f0d27c1b811dd6ad39758bea4c8f40/invoke.js"
+        src="//chickenadjacent.com/2c5e03b2597c3bd82333c06d7e9dd6e0/invoke.js"
       />
     </div>
   );
