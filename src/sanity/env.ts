3e1ff52114d406c0b1d2e10d0a9cd98ab5436bae
Uduafemhe Favour <uduafemhe.A.favour@gmail.com>
2025-09-12 14:59:34 +0000

    The Netlify deploy errored, with the following guidance provided:
    ### D

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,9 +1,9 @@
 
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-07-22'
 
-// This file is used for both server-side and client-side operations.
-// Vercel exposes environment variables to the build process via `process.env`.
-// We prioritize the server-side variables and fall back to the public ones.
-export const dataset = process.env.SANITY_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.SANITY_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+// When fetching in a server-side context (like getStaticProps),
+// Vercel exposes environment variables through process.env.
+// NEXT_PUBLIC_ is for client-side exposure.
+export const dataset = 'production';
+export const projectId = 'qbb85k0a';
 export const useCdn = false
