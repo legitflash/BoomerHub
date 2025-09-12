60ca84a5b60e0b2eaeea42198e4e847728e95f84
Uduafemhe Favour <uduafemhe.A.favour@gmail.com>
2025-09-12 14:51:35 +0000

    [15:42:49.974] Running build in Washington, D.C., USA (East) – iad1 [15:

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,6 +1,9 @@
 
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-07-22'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+// When fetching in a server-side context (like getStaticProps),
+// Vercel exposes environment variables through process.env.
+// NEXT_PUBLIC_ is for client-side exposure.
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET || 'production';
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID!;
 export const useCdn = false
