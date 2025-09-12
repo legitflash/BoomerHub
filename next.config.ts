188b83e27622c1ac4b54a5cddb20dd808ed52a55
Uduafemhe Favour <uduafemhe.A.favour@gmail.com>
2025-09-12 00:08:21 +0000

    Next

--- next.config.ts
+++ next.config.ts
@@ -59,7 +59,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'unsplash.com',
         port: '',
-        pathname: '/**',
+pathname: '/**',
       },
       {
         protocol: 'https',
