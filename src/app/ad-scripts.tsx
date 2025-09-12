2a3345abca17b99060968a2c50a4237a4f569085
Uduafemhe Favour <uduafemhe.A.favour@gmail.com>
2025-09-12 00:11:12 +0000

    Remove social bar ads

--- src/app/ad-scripts.tsx
+++ src/app/ad-scripts.tsx
@@ -14,19 +14,10 @@ export default function AdScripts() {
     popUnder.referrerPolicy = 'no-referrer-when-downgrade';
     document.body.appendChild(popUnder);
 
-    // Adsterra Social Bar Script
-    const socialBar = document.createElement('script');
-    socialBar.type = 'text/javascript';
-    socialBar.src = '//chickenadjacent.com/f4/bf/1b/f4bf1ba199807a6dcc09287c2ec74dd5.js';
-    document.body.appendChild(socialBar);
-
     return () => {
       // Cleanup scripts on component unmount
       if (document.body.contains(popUnder)) {
         document.body.removeChild(popUnder);
-      }
-      if (document.body.contains(socialBar)) {
-        document.body.removeChild(socialBar);
       }
     };
   }, []);
